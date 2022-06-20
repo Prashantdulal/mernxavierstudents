e78c32c44f8402f0785591260dfad2ab1e3d8d00
Prashantdulal <prashantdulal3@gmail.com>
2022-06-21 03:01:33 +0545

    make it better

--- app.js
+++ app.js
@@ -24,13 +24,31 @@ app.get("/users", (req, res) => {
   res.send("This is users");
 });
 
-const PORT  = process.env.PORT || 3000;
+const PORT  = process.env.PORT || 5000;
 
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
   
 }
+
+ 
+
+if ( process.env.NODE_ENV == "production"){
+
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+  })
+
+
+}
+
 
 app.listen(PORT, () => {
   console.log(`serverr runningg!!`);
